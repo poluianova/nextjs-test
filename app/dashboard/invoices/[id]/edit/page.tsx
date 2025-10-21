f4c87703abd5452677b0c9df4e028e1512de071e
maryna <maryna@cloudomation.com>
2025-10-21 12:19:20 +0200

    implement error boundary and notFound

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -1,13 +1,14 @@
 import Form from '@/app/ui/invoices/edit-form';
 import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
 import { fetchCustomers, fetchInvoiceById } from '@/app/lib/data';
+import { notFound } from 'next/navigation';
  
 export default async function Page(props: {params: Promise<{id: string}>}) {
     const params = await props.params;
     const id = params.id;
 
     const [invoice, customers] = await Promise.all([fetchInvoiceById(id), fetchCustomers()])
-  return (
+  return (!invoice ? notFound() : 
     <main>
       <Breadcrumbs
         breadcrumbs={[
